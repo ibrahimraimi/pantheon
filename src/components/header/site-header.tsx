db53462d7ab29825b4299bf8334fc82c4c3600b6
Ibrahim Raimi <raimiibrahim44@gmail.com>
2025-12-17 16:58:11 +0100

    feat: add ShoppingBag component for cart management and UI enhancements
    - Implement ShoppingBag component to display cart items, subtotal, and checkout options.
    - Create StatusBar component for displaying promotional messages.
    - Add ScrollToTop component to reset scroll position on route changes.
    - Introduce Accordion component for collapsible content sections.
    - Develop Badge and Button components for UI consistency.
    - Implement Toast and Toaster components for user notifications.
    - Create useIsMobile and useToast hooks for responsive design and toast management.
    - Update Tailwind CSS configuration for improved styling and animations.

---
+++ src/components/header/site-header.tsx
@@ -0,0 +1,13 @@
+import StatusBar from "./status-bar";
+import Navigation from "./navigation";
+
+const SIteHeader = () => {
+  return (
+    <header className="w-full sticky top-0 z-50">
+      {/* <StatusBar /> */}
+      <Navigation />
+    </header>
+  );
+};
+
+export default SIteHeader;
